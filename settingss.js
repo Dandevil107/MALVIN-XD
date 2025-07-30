8aa4370b2179be9f98382c97d0e1d4d636a10e3e
Dandevil107 <dandevil107@gmail.com>
2025-07-30 18:18:29 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "false",malvin~s29VhJQD#gkLE27IKsw8beOqeDq0iXHqDGsYL0EynJhZdZyGyilA
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
